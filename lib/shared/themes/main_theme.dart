6f2c2bf0c74dcf3b12714368da8d38a9d27c75ca
nezarab404 <nezar.ab.23@gmail.com>
2021-12-04 10:31:19 -0500

    setting up colors and main theme

--- lib/shared/themes/main_theme.dart
+++ lib/shared/themes/main_theme.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
 
-const mainRed = Color(0xffDC1A45);
-const mainDarkBlue = Color(0xff3C3D51);
+Map<int, Color> color = const {
+  50: Color.fromRGBO(136, 14, 79, .1),
+  100: Color.fromRGBO(136, 14, 79, .2),
+  200: Color.fromRGBO(136, 14, 79, .3),
+  300: Color.fromRGBO(136, 14, 79, .4),
+  400: Color.fromRGBO(136, 14, 79, .5),
+  500: Color.fromRGBO(136, 14, 79, .6),
+  600: Color.fromRGBO(136, 14, 79, .7),
+  700: Color.fromRGBO(136, 14, 79, .8),
+  800: Color.fromRGBO(136, 14, 79, .9),
+  900: Color.fromRGBO(136, 14, 79, 1),
+};
+
+MaterialColor? mainRed = MaterialColor(0xffDC1A45, color);
+MaterialColor? mainDarkBlue = MaterialColor(0xff3C3D51,color);
+
+
+
+//0xffDC1A45
